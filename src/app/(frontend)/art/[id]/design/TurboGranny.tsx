d020145a5b7702d1d2fee163edb6dfe0acaea79e
Mitchell Christ <xzmaru@gmail.com>
2024-12-08 18:14:31 -0800

    TG adjustments

--- src/app/(frontend)/art/[id]/design/TurboGranny.tsx
+++ src/app/(frontend)/art/[id]/design/TurboGranny.tsx
@@ -1,14 +1,13 @@
-import { cn } from '@/lib/utils'
 import Img from '@/ui/Img'
 import Text from '@/ui/Text'
 
 export default function TurboGranny({ design }: { design: Sanity.Design }) {
 	return (
 		<>
-			<div className="pointer-events-none absolute inset-0 bg-[#af1421] opacity-50 mix-blend-color-burn" />
+			<div className="pointer-events-none absolute inset-0 bg-[#af1421] opacity-20 mix-blend-color-burn" />
 
 			<div className="relative flex h-full flex-col items-end pr-[20vw] pb-[3vw] text-right font-serif">
-				<div className="z-1 mr-[-9vw] mb-auto translate-y-[-3vw] opacity-30 mix-blend-color-dodge grayscale [mask-image:linear-gradient(to_bottom,black_50%,transparent)]">
+				<div className="z-1 mr-[-9vw] mb-auto translate-y-[-3vw] opacity-40 mix-blend-color-dodge grayscale [mask-image:linear-gradient(to_bottom,black_50%,transparent)]">
 					<Img
 						className="w-[58vw]"
 						image={design.assets?.[0]}
@@ -28,15 +27,8 @@ export default function TurboGranny({ design }: { design: Sanity.Design }) {
 				/>
 			</div>
 
-			<h2
-				className={cn(
-					'absolute top-1/2 right-0 -translate-y-1/2 whitespace-nowrap',
-					'font-dela text-[16.3vw] leading-tight tracking-[-.15em] [writing-mode:vertical-rl]',
-					'mix-blend-difference',
-				)}
-			>
+			<h2 className="font-dela absolute top-1/2 right-0 -translate-y-1/2 text-[16.3vw] leading-tight tracking-[-.15em] whitespace-nowrap opacity-90 mix-blend-difference blur-[1px] [writing-mode:vertical-rl]">
 				ターボババア
-				<div className="absolute inset-0 backdrop-blur [mask-image:linear-gradient(to_right,transparent,black_60%)]" />
 			</h2>
 		</>
 	)
